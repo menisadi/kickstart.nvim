bdb305f9c79749388257133970c041b5d640203c
menisadi <menisadi@gmail.com>
2024-02-05 18:17:23 +0200

    subplots snippet

--- lua/custom/snippets/python.lua
+++ lua/custom/snippets/python.lua
@@ -3,6 +3,16 @@ local s = ls.snippet
 
 return {
 	s(
+		{ trig = "subplots" },
+		{
+			t("fig, axs = plt.subplots(nrows="),
+			i(1),
+			t(", ncols="),
+			i(2),
+			t(")")
+		}
+	),
+	s(
 		{ trig = "pandas" },
 		{ t({ "import pandas as pd" }) }
 	),
