ff1922f1953395e3d4b39260c8849890e91ffad8
menisadi <menisadi@gmail.com>
2024-02-21 19:01:39 +0200

    animate - testing it

--- init.lua
+++ init.lua
@@ -305,6 +305,9 @@ vim.keymap.set(
 -- [[ Oil ]]
 require("oil").setup()
 
+-- [[ Mini.Animate ]]
+require('mini.animate').setup()
+
 -- [[ Python ]]
 -- Help nvim find python so it won't take it extra second to find it by it self
 vim.g.python3_host_prog = '/Users/meni/.pyenv/versions/nvim/bin/python3'
