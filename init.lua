12fd0e3b203936f32878fbaf345cbcfaac7fd73c
menisadi <menisadi@gmail.com>
2024-04-08 01:07:00 +0300

    startuptime - temp and zoxide - maybe not temp

--- init.lua
+++ init.lua
@@ -401,6 +401,8 @@ require('telescope').setup {
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 require("telescope").load_extension("aerial")
+require("telescope").load_extension('zoxide')
+
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
@@ -420,6 +422,10 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+
+-- Adding zoxide list keybinding
+vim.keymap.set("n", "<leader>sz", require("telescope").extensions.zoxide.list,
+  { desc = '[S]search directories using [z]oxide' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
