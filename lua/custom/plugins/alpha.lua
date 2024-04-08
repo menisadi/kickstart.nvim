d190e93a7e720510446b689158e4f425d9bd66ce
menisadi <menisadi@gmail.com>
2024-04-08 11:37:02 +0300

    alpha is done :)

--- lua/custom/plugins/alpha.lua
+++ lua/custom/plugins/alpha.lua
@@ -6,13 +6,10 @@ return {
 		dashboard.section.header.val = { " Hi there " }
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button("SPC w o", " Root folder", "<CMD>Oil ~ <CR>"),
-			dashboard.button("SPC s f", "󰐱 Fuzzy find file",
-				require('telescope.builtin').find_files),
-			dashboard.button("SPC s z", " Zoxide list",
-				require('telescope').extensions.zoxide.list),
-			dashboard.button("SPC ?", "󰋚 Recently opened files",
-				require('telescope.builtin').oldfiles),
+			dashboard.button("SPC w o", " Root folder"),
+			dashboard.button("SPC s f", "󰐱 Fuzzy find file"),
+			dashboard.button("SPC s z", " Zoxide list"),
+			dashboard.button("SPC ?", "󰋚 Recently opened files"),
 			dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
 		}
 		dashboard.config.opts.noautocmd = true
