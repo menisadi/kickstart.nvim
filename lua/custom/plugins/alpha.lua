53bdcd3345986a81b178e368c2d80fcbd10b53f3
menisadi <menisadi@gmail.com>
2024-04-10 21:37:58 +0300

    start screen - cat

--- lua/custom/plugins/alpha.lua
+++ lua/custom/plugins/alpha.lua
@@ -3,13 +3,19 @@ return {
 	config = function()
 		local alpha = require 'alpha'
 		local dashboard = require 'alpha.themes.dashboard'
-		dashboard.section.header.val = { " Hi there " }
+		dashboard.section.header.val = {
+			"  ／l、             ",
+			"（ﾟ､ ｡ ７         ",
+			"  l  ~ヽ       ",
+			"  じしf_,)ノ",
+			"Well hello again"
+		}
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button("SPC w o", " Root folder"),
-			dashboard.button("SPC s f", "󰐱 Fuzzy find file"),
-			dashboard.button("SPC s z", " Zoxide list"),
-			dashboard.button("SPC ?", "󰋚 Recently opened files"),
+			dashboard.button("SPC w o", "  Root folder"),
+			dashboard.button("SPC s f", "󰐱  Fuzzy find file"),
+			dashboard.button("SPC s z", "  Zoxide list"),
+			dashboard.button("SPC ?", "󰋚  Recently opened files"),
 			dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
 		}
 		dashboard.config.opts.noautocmd = true
